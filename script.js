5fa91dc7be8194e399998e96c53056837a8f8259
Vibhav <vibhavydm@gmail.com>
2026-01-30 09:54:09 +0530

    implemented search option

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('product-container');
+    const searchInput = document.getElementById('searchInput');
+    const searchButton = document.getElementById('searchBtn');
+    let products = {};
+
 
     fetch('https://dummyjson.com/products')
         .then(res => res.json())
         .then(data => {
+            products = data.products;
             container.innerHTML = '';
             data.products.forEach(product => {
                 const card = document.createElement('div');
@@ -19,4 +24,29 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(err => {
             console.error(err);
         });
+
+    searchButton.addEventListener('click', (e) => {
+        const query = searchInput.value.toLowerCase();
+        const filteredProducts = fetch('https://dummyjson.com/products/search?q=' + query)
+            .then(res => res.json())
+            .then( data => {
+                products = data.products;
+                container.innerHTML = '';
+                data.products.forEach(product => {
+                    const card = document.createElement('div');
+                    card.className = 'product-card';
+                    card.innerHTML = `
+                        <img src="${product.thumbnail}" alt="${product.title}">
+                        <h3>${product.title}</h3>
+                        <p class="price">$${product.price}</p>
+                    `;
+                    container.appendChild(card);
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            return [];
+        });
+    });
 });
+//
